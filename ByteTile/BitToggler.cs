d678d9102f18d993790c8aa8de199e5642603dac
SDBagel <35588499+SDBagel@users.noreply.github.com>
2018-10-26 19:04:53 -0700

    Add BitToggler
    Nothing really does anything atm.

---
+++ ByteTile/BitToggler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ByteTile
+{
+    public partial class BitToggler : UserControl
+    {
+        public int column = 0;
+        public int row = 0;
+        public bool isToggled = false;
+
+        public BitToggler()
+        {
+            InitializeComponent();
+        }
+
+        private void BitToggler_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (BackColor.Equals(Color.Black))
+            {
+                BackColor = Color.White;
+                isToggled = false;
+            }
+            else
+            {
+                BackColor = Color.Black;
+                isToggled = true;
+            }
+                
+        }
+    }
+}
--- ByteTile/Form1.Designer.cs
+++ ByteTile/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bitToggler1 = new ByteTile.BitToggler();
+            this.SuspendLayout();
+            // 
+            // bitToggler1
+            // 
+            this.bitToggler1.BackColor = System.Drawing.Color.White;
+            this.bitToggler1.Location = new System.Drawing.Point(12, 12);
+            this.bitToggler1.Name = "bitToggler1";
+            this.bitToggler1.Size = new System.Drawing.Size(40, 40);
+            this.bitToggler1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(87)))), ((int)(((byte)(114)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.bitToggler1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private BitToggler bitToggler1;
     }
 }
 
